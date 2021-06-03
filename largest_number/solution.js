845ef52b603570b09c5d4a8573a9c4f3c325fe5b
trieule50 <83842451+trieule50@users.noreply.github.com>
2021-06-03 10:08:48 -0400

    Update solution.js

--- largest_number/solution.js
+++ largest_number/solution.js
@@ -1,1 +1,11 @@
+const largestNumber = (num) => {
+    if(num === 0){
+        return 0;
+    }else{
+    const myString = '9';
+    let stringNum = myString.repeat(num);
+    return parseInt(stringNum);
+    }
+}
 
+console.log(largestNumber(5));
