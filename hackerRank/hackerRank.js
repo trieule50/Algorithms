e8be5f793711f4d3bb3b29b67442ecb3a89cde4f
trieule50 <trieule50@gmail.com>
2021-08-20 19:29:19 -0400

    Complete make Array Consecutive

--- hackerRank/hackerRank.js
+++ hackerRank/hackerRank.js
@@ -42,3 +42,23 @@ function firstNotRepeatingCharacter(s) {
 }
 
 // console.log(firstNotRepeatingCharacter(str))
+
+// Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue having an non-negative integer size. Since he likes to make things perfect, he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. He may need some additional statues to be able to accomplish that. Help him figure out the minimum number of additional statues needed.
+
+const statues1 = [6, 2, 3, 8]
+
+function makeArrayConsecutive2(statues) {
+    const sorted = statues.sort((a,b) => a-b)
+    let min = sorted[0]
+    let max = sorted[sorted.length -1]
+    let count = 0;
+    for(let i=min; i < max; i++){
+        console.log(sorted.indexOf(i))
+        if(sorted.indexOf(i) === -1){
+            count+=1
+        }
+    }
+    return count
+}
+
+// console.log(makeArrayConsecutive2(statues1))
