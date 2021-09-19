3af50d2c9eda6ebe254c49f5c07ac45c2c7618fa
trieule50 <trieule50@gmail.com>
2021-09-19 19:06:08 -0400

    Digonal difference

--- hackerRank/hackerRank.js
+++ hackerRank/hackerRank.js
@@ -41,6 +41,20 @@ function firstNotRepeatingCharacter(s) {
     
 }
 
+
+
+function lonelyinteger(a) {
+    // Write your code here
+    for(let i=0; i< a.length; i++){
+        if(a.includes(a[i])){
+            return a
+        }
+    }
+
+}
+
+// console.log(lonelyinteger([1,2,3,4,3,2,1]))
+
 // console.log(firstNotRepeatingCharacter(str))
 
 // Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue having an non-negative integer size. Since he likes to make things perfect, he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. He may need some additional statues to be able to accomplish that. Help him figure out the minimum number of additional statues needed.
@@ -81,4 +95,115 @@ function almostIncreasingSequence(sequence) {
     return error <= 1
 }
 
-console.log(almostIncreasingSequence(sequence1))
+// console.log(almostIncreasingSequence(sequence1))
+
+// You are given an n x n 2D matrix that represents an image. Rotate the image by 90 degrees (clockwise).
+
+// const a1 = [[1, 2, 3],
+//      [4, 5, 6],
+//      [7, 8, 9]]
+
+// function rotateImage(a) {
+
+// }
+
+// console.log(rotateImage(a1))
+
+
+function mutateTheArray(n,a){
+    let arr = []
+    for(let i=0; i < a.length; i++){
+        for(let j=0; j < n; j++){
+            if(!a[i+1]){
+                arr.push(a[i])
+            }else{
+                let b = a[i] + a[i+1]
+                console.log(arr.push(b))
+            }
+        }
+    }
+    return arr
+
+}
+
+// console.log(mutateTheArray(1,[9]))
+
+let arr2 = [25, 2, 3, 57, 38, 41]
+
+function mostFrequentDigits(a){
+    let sorted = a.join('').split('')
+    let count = sorted.reduce((counts, num) => {
+        counts[num] = (counts[num] || 0) + 1
+        return counts
+    }, {})
+    return count
+}
+
+// console.log(mostFrequentDigits(arr2))
+
+const blocks1 = [1,3,1,3,3]
+
+function packBlocks(blocks, height){
+    const sorted = blocks.sort((a,b)=> a-b)
+    console.log(sorted)
+    let length = sorted.length-1
+    for(let i = 0; i < length; i++){
+        if(length === 1){
+            return 
+        }
+        return sorted[length] + sorted[length-1]
+    }
+}
+
+// console.log(packBlocks(blocks1, 2))
+
+// Given an array of integers, where all elements but one occur twice, find the unique element.
+
+const ar = [1,2,3,4,3,2,1];
+
+function lonelyinteger(a){
+    for(let i = 0; i < a.length-1; i++){
+        for(let j=i+1; j < a.length; j++){
+            if(a[i] !== a[j]){
+                return a[i]
+            }
+        }
+    }
+    
+}
+
+// console.log(lonelyinteger(ar))
+
+
+function gameWinner(colors) {
+    // Write your code here
+    let w = 0;
+    let b = 0;
+    for(let i = 0; i <= colors.length; i++){
+        // console.log(colors[i])
+     if(colors[i] === 'w'){
+         w++
+         console.log(w)
+     }else{
+         b++
+     }   
+    }
+    if(w >= b){
+        return 'wendy'
+    }else{
+        return 'bob'
+    }
+}
+
+// console.log(gameWinner('wwwbb'))
+
+
+function oddNumbers(l,r){
+    for(let i = l; i <=r; i++){
+        if(i % 2 !== 0){
+            console.log(i)
+        }
+    }
+}
+
+console.log(oddNumbers(3,9))
